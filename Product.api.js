28e51ee458f21abc7a44bfabc12b40135a7c0cce
bakkarmmm <moumoubakkar@gmail.com>
2026-03-16 00:46:53 +0200

    reOrder Category

--- Product.api.js
+++ Product.api.js
@@ -18,34 +18,79 @@ router.get("/item/:id", async (req, res) => {
     res.status(500).json(error);
   }
 });
+// router.get("/:resturantSlug", async (req, res) => {
+//   const { resturantSlug } = req.params;
+//   try {
+//     const resturant = await Busninss.findOne({ slug: resturantSlug });
+//     const items = await Item.find({ bussnins_id: resturant._id }).populate({
+//       path: "gategoryID",
+//       select: "name",
+//     });
+//     const Promos = await Promo.find({ bussninsId: resturant._id ,isActive:true});
+//     const categoriesIds = await Item.find({
+//       bussnins_id: resturant._id,
+//     }).distinct("gategoryID");
+//     const restaurantName = items.length > 0 ? items[0].ResturantSlug : null;
+//     const categories = await Category.find({ _id: { $in: categoriesIds },isActive:true }).sort({ order: 1 });
+//     const bussnise = await Busninss.find({ slug: resturantSlug }).populate({
+//       path: "type",
+//       select: "name",
+//     });
+//     res.json({
+//       menu: items,
+//       categris: categories,
+//       RestaurantNames: restaurantName,
+//       bussnise: bussnise,
+//       Promos:Promos
+//     });
+//   } catch (err) {
+//     res.status(500).json({ message: err.message });
+//   }
+// });
 router.get("/:resturantSlug", async (req, res) => {
   const { resturantSlug } = req.params;
+  const page = parseInt(req.query.page) || 1;  // الصفحة الحالية
+  const limit = parseInt(req.query.limit) || 10; // عدد المنتجات لكل صفحة
+  const skip = (page - 1) * limit;
+
   try {
     const resturant = await Busninss.findOne({ slug: resturantSlug });
-    const items = await Item.find({ bussnins_id: resturant._id }).populate({
-      path: "gategoryID",
-      select: "name",
-    });
+    if(!resturant) return res.status(404).json({ message: "Restaurant not found" });
+
+    const totalItems = await Item.countDocuments({ bussnins_id: resturant._id });
+
+    const items = await Item.find({ bussnins_id: resturant._id })
+      .populate({ path: "gategoryID", select: "name" })
+      .skip(skip)
+      .limit(limit);
+
     const Promos = await Promo.find({ bussninsId: resturant._id ,isActive:true});
+
     const categoriesIds = await Item.find({
       bussnins_id: resturant._id,
     }).distinct("gategoryID");
+
     const restaurantName = items.length > 0 ? items[0].ResturantSlug : null;
-    const categories = await Category.find({ _id: { $in: categoriesIds },isActive:true }).sort({ order: 1 });
+
+    const categories = await Category.find({ _id: { $in: categoriesIds }, isActive:true }).sort({ order: 1 });
+
     const bussnise = await Busninss.find({ slug: resturantSlug }).populate({
       path: "type",
       select: "name",
     });
+
     res.json({
       menu: items,
       categris: categories,
       RestaurantNames: restaurantName,
       bussnise: bussnise,
-      Promos:Promos
+      Promos: Promos,
+      total: totalItems, // عدد المنتجات الكلي
+      page,
+      limit
     });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
 });
-
 export default router;
